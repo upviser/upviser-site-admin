d3e9d22356321d96f84c54c6e194aff9b6e68f6f
upvisor <admin@upvisor.cl>
2025-07-05 20:36:05 -0400

    actualization general

--- app/configuracion/integraciones/page.tsx
+++ app/configuracion/integraciones/page.tsx
@@ -45,62 +45,59 @@ export default function Page () {
 
   return (
     <>
-      <Head>
-        <title>Integraciones</title>
-      </Head>
-        <div className='fixed flex bg-white border-t bottom-0 right-0 p-4 w-full lg:w-[calc(100%-250px)] dark:bg-neutral-800 dark:border-neutral-700'>
-          <div className='flex m-auto w-full max-w-[1280px]'>
-            {
-              error !== ''
-                ? <p className='px-2 py-1 bg-red-500 text-white w-fit h-fit my-auto'>{ error }</p>
-                : ''
-            }
-            <div className='flex gap-6 ml-auto w-fit'>
-              <ButtonSubmit action={handleSubmit} color='main' submitLoading={loading} textButton='Guardar datos' config='w-40' />
-              <button onClick={() => router.refresh()} className='my-auto text-sm'>Descartar</button>
+      <div className='fixed flex bg-white border-t bottom-0 right-0 p-4 w-full lg:w-[calc(100%-250px)] dark:bg-neutral-800 dark:border-neutral-700'>
+        <div className='flex m-auto w-full max-w-[1280px]'>
+          {
+            error !== ''
+              ? <p className='px-2 py-1 bg-red-500 text-white w-fit h-fit my-auto'>{ error }</p>
+              : ''
+          }
+          <div className='flex gap-6 ml-auto w-fit'>
+            <ButtonSubmit action={handleSubmit} color='main' submitLoading={loading} textButton='Guardar datos' config='w-40' />
+            <button onClick={() => router.refresh()} className='my-auto text-sm'>Descartar</button>
+          </div>
+        </div>
+      </div>
+      <div className='p-4 lg:p-6 w-full flex flex-col gap-6 overflow-y-auto bg-bg dark:bg-neutral-900 mb-16' style={{ height: 'calc(100% - 73px)' }}>
+        <div className='flex w-full max-w-[1280px] mx-auto gap-6 flex-col lg:flex-row'>
+          <Nav />
+          <div className='w-full lg:w-3/4 flex flex-col gap-4'>
+            <h2 className='font-medium mt-3 pb-3 border-b dark:border-neutral-700'>Integraciones</h2>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>ID del número de teléfono</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, idPhone: e.target.value })} value={integrations.idPhone} placeholder='ID del número de teléfono' config='h-40' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>Token Whatsapp App</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, whatsappToken: e.target.value })} value={integrations.whatsappToken} placeholder='Api Meta Token' config='h-40' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>ID de la página de Facebook</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, idPage: e.target.value })} value={integrations.idPage} placeholder='ID de la página de Facebook' config='h-40' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>ID del Instagram</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, idInstagram: e.target.value })} value={integrations.idInstagram} placeholder='ID del Instagram' config='h-40' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>Token Messenger App</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, messengerToken: e.target.value })} value={integrations.messengerToken} placeholder='Api Meta Token' config='h-40' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>Api Meta Token</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, apiToken: e.target.value })} value={integrations.apiToken} placeholder='Api Meta Token' config='h-40' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>Api Pixel Id</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, apiPixelId: e.target.value })} value={integrations.apiPixelId} placeholder='Api Pixel Id' config='h-40' />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <h3 className='text-sm'>Google Analytics</h3>
+              <Input change={(e: any) => setIntegrations({ ...integrations, googleAnalytics: e.target.value })} value={integrations.googleAnalytics} placeholder='Google Analytics' config='h-40' />
             </div>
           </div>
         </div>
-        <div className='p-4 lg:p-6 w-full flex flex-col gap-6 overflow-y-auto bg-bg dark:bg-neutral-900 mb-16' style={{ height: 'calc(100% - 73px)' }}>
-          <div className='flex w-full max-w-[1280px] mx-auto gap-6 flex-col lg:flex-row'>
-            <Nav />
-            <div className='w-full lg:w-3/4 flex flex-col gap-4'>
-              <h2 className='font-medium mt-3 pb-3 border-b dark:border-neutral-700'>Integraciones</h2>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>ID del número de teléfono</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, idPhone: e.target.value })} value={integrations.idPhone} placeholder='ID del número de teléfono' config='h-40' />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>Token Whatsapp App</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, whatsappToken: e.target.value })} value={integrations.whatsappToken} placeholder='Api Meta Token' config='h-40' />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>ID de la página de Facebook</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, idPage: e.target.value })} value={integrations.idPage} placeholder='ID de la página de Facebook' config='h-40' />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>ID del Instagram</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, idInstagram: e.target.value })} value={integrations.idInstagram} placeholder='ID del Instagram' config='h-40' />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>Token Messenger App</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, messengerToken: e.target.value })} value={integrations.messengerToken} placeholder='Api Meta Token' config='h-40' />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>Api Meta Token</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, apiToken: e.target.value })} value={integrations.apiToken} placeholder='Api Meta Token' config='h-40' />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>Api Pixel Id</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, apiPixelId: e.target.value })} value={integrations.apiPixelId} placeholder='Api Pixel Id' config='h-40' />
-              </div>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-sm'>Google Analytics</h3>
-                <Input change={(e: any) => setIntegrations({ ...integrations, googleAnalytics: e.target.value })} value={integrations.googleAnalytics} placeholder='Google Analytics' config='h-40' />
-              </div>
-            </div>
-          </div>
-        </div>
+      </div>
     </>
   )
 }
