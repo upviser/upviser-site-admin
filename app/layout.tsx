6db8ea9f7dadd2494e51a101565578d001d9666e
upvisor <admin@upvisor.cl>
2025-07-20 19:37:55 -0400

    actualization general

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Navbar } from '@/components/layouts'
 import { ThemeProvider } from 'next-themes'
 import { LeftMenu } from '@/components/ui'
 import localFont from 'next/font/local'
+import Script from 'next/script'
 
 const myFont = localFont({
   src: './fonts/RedHatDisplay-VariableFont_wght.ttf',
@@ -23,6 +24,33 @@ export default function RootLayout({
         <link rel="icon" href={process.env.NEXT_PUBLIC_FAVICON} />
       </head>
       <body>
+        <div id="fb-root" />
+        <Script
+          id="facebook-sdk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.fbAsyncInit = function() {
+                FB.init({
+                  appId      : '${process.env.NEXT_PUBLIC_FB_APP_ID}',
+                  cookie     : true,
+                  xfbml      : true,
+                  version    : 'v20.0'
+                });
+                console.log('✅ FB.init ejecutado');
+              };
+              (function(d, s, id) {
+                if (d.getElementById(id)) return;
+                var js = d.createElement(s);
+                js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk.js';
+                js.async = true;
+                js.defer = true;
+                d.head.appendChild(js);
+              })(document, 'script', 'facebook-jssdk');
+            `,
+          }}
+        />
         <SessionProvider>
           <ThemeProvider attribute='class'>
             <Navbar>
